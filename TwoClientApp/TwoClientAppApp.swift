52e5afaab5b3a5b0225a3a12f190f0874489bf05
Lenny Burdette <lenny@apollographql.com>
2023-05-09 10:21:13 -0400

    Initial Commit

---
+++ TwoClientApp/TwoClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoClientAppApp.swift
+//  TwoClientApp
+//
+//  Created by Lenny Burdette on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TwoClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
